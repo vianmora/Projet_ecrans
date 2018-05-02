eba582204f536cb2fd251909a375fdc4bb49d4f8
vianmora <vianmora@yahoo.fr>
2018-05-02 14:48:39 +0200

    retour à une simple page écran en html. trouvons comment modifier son message à partir d'un fichier texte

--- app.js
+++ app.js
@@ -15,19 +15,12 @@ app.use('/assets' /*lien à appeler*/, express.static('assets' /*lien réel*/));
 // récupérer les données
 app.use(bodyparser.urlencoded({ extended: false }));
 
-var app_admin = require('./routes/admin');
-app.use('/page-admin', app_admin);
-
-var app_ecran = require('./routes/ecran');
-app.use('/ecran', express.static('static/page-ecran/index.html'));
-
-
-/*router des pages avec le moteur de template*/
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/uploads/index.html');
 });
 
+var app_admin = require('./routes/admin');
+app.use('/page-admin', app_admin);
 
 /* ajout d'une condition 404 avec un middleware*/
 app.use(function (req, res, next) {
