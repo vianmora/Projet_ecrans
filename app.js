9fb9a9e11fd4d20c0a5bfceaf6d14312e26efb02
vianmora <vianmora@yahoo.fr>
2018-05-02 14:13:40 +0200

    épuration des pages

--- app.js
+++ app.js
@@ -16,36 +16,18 @@ app.use('/assets' /*lien à appeler*/, express.static('assets' /*lien réel*/));
 app.use(bodyparser.urlencoded({ extended: false }));
 
 var app_admin = require('./routes/admin');
+app.use('/page-admin', app_admin);
 
-/* premier routage */
+var app_ecran = require('./routes/ecran');
+app.use('/ecran', express.static('static/page-ecran/index.html'));
+
 
 /*router des pages avec le moteur de template*/
-app.get('/Home', function(req, res){
-  res.render('Home');
-});
 
 app.get('/', function(req, res){
-  res.render('Home');
+  res.sendFile(__dirname + '/views/uploads/index.html');
 });
 
-app.get('/Page-ecran', function(req, res){
-  res.sendFile(__dirname + '/views/uploads/salut.html', {message : global.message});
-});
-
-
-/* gérer la page contact avec des requète en query string et POST */
-
-app.route('/contact')
-  .get(function(req, res){
-    res.render('Contact');
-  })
-  .post(function(req, res){
-    if (!req.body) return res.render('Contact', {qs:req.query});
-    res.render('Contact-success', {POST : req.body});
-  });
-
-/* routage parallèle pour la page admin*/
-app.use('/Page-admin', app_admin);
 
 /* ajout d'une condition 404 avec un middleware*/
 app.use(function (req, res, next) {
