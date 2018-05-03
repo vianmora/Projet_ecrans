1d8db3d8c857384c985e9f76e9c88ebc2cdaa49f
vianmora <vianmora@yahoo.fr>
2018-05-03 22:31:51 +0200

    correction des menus dans la page admin

--- modules.js/logger.js
+++ modules.js/logger.js
@@ -4,16 +4,16 @@ const logger = winston.createLogger(
   {format: winston.format.combine(winston.format.timestamp(), winston.format.simple())}
 );
 
-/* pour le mode dev
+/* pour le mode dev*/
 
 logger.add(new winston.transports.Console(
   {level: 'silly'}
-));*/
+));
 
 
-/* pour avoir un fichier log */
+/* pour avoir un fichier log 
 
 logger.add(new winston.transports.File({ filename: './log/error.log', level: 'error' }));
-logger.add(new winston.transports.File({ filename: './log/combined.log', level: 'silly'}));
+logger.add(new winston.transports.File({ filename: './log/combined.log', level: 'silly'}));*/
 
 module.exports = logger;
