05cf05d6f767dd0abba1d22f6b128a0db74e30b0
Adrien PILLET <adrien.pillet@centrale-marseille.fr>
2018-05-04 14:00:09 +0200

    pull du push de vianney

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var logger = require('./modules.js/logger');
-var serveur = require('./modules.js/serveur.js');
+var app = require('./modules.js/serveur.js');
 
 var port = 3000;
 
-serveur.listen(port); // Ajout d'un port d'écoute pour la version en dévelopement
+app.listen(port); // Ajout d'un port d'écoute pour la version en dévelopement
 
 //petit message sympa ;)
 
 console.log("c'est bon on t'écoute sur le port " + port.toString());
 
-<<<<<<< HEAD
 // récupérer les données
 app.use(bodyparser.urlencoded({ extended: false }));
 
@@ -36,10 +35,10 @@ app.use(function (req, res, next) {
 app.listen (3000); // Ajout d'un port d'écoute pour la version en dévelopement
 
 console.log("c'est bon, je t'écoute sur le port 3000"); //petit message sympa ;)*
-=======
+
 logger.info("début du log général, on écoute sur le port " + port.toString());
 logger.silly("début du log précis en mode 'silly'");
->>>>>>> 1d8db3d8c857384c985e9f76e9c88ebc2cdaa49f
+
 
 /*dézippe
 message simple
